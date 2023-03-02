b1b885fd543a23ebb8a807b2a791a52236e6524f
aysenuryazici9 <aysnryzc129@gmail.com>
2023-03-02 22:57:28 +0300

    Ogrenci_Kayit
    Ogrenci_Kayit

---
+++ MvcOgrenciKayit/Controllers/TablesController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcOgrenciKayit.Models;
+
+namespace MvcOgrenciKayit.Controllers
+{
+    public class TablesController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+        public ActionResult Index()
+        {
+            return View(db.Table.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OgrID,OgrAd,OgrSoyad,OgrMail,OgrFotograf,OgrAdres")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OgrID,OgrAd,OgrSoyad,OgrMail,OgrFotograf,OgrAdres")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Table.Find(id);
+            db.Table.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
